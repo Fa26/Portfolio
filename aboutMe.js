6f768e231cc079552b58c3e8c979e0ae1d64d990
Fa26 <fa@MacBook-Pro-de-Fatima.local>
2024-07-22 16:21:25 -0500

    aboutMe

--- aboutMe.js
+++ aboutMe.js
@@ -10,6 +10,6 @@ function setup() {
   function draw() { 
 	//get the pencil effect by using mouseX/Y and pmouseX/Y (previous) inbuilt positions connecting where the mouse current is and where it was
 	line(mouseX, mouseY, pmouseX, pmouseY);//line command takes 4 arguments starting x&y and ending x&y
-	// ellipse(mouseX, mouseY, 20, 20);
+	//ellipse(mouseX, mouseY, 20, 20);
 	  
   }
