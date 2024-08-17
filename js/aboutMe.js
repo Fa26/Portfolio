029cbe8db6ea24d0f51a33ec415387f8c721bd63
Fa26 <fa@MacBook-Pro-de-Fatima.local>
2024-08-16 20:40:13 -0500

    changes styles

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -1,11 +1,3 @@
-var today= new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-
-
-today = mm + '/' + dd + '/' + yyyy;
-var todayDate= document.getElementById("dateNow").innerHTML(today)
 
 
 function setup() { 
@@ -17,12 +9,42 @@ function setup() {
 	
 	// noStroke();
 	 fill(0, 102);//transparent
-  
+	 const edu = document.getElementById("edu").addEventListener("click",()=>{
+		education()
+	})
+	const teck = document.getElementById("tech").addEventListener("click",()=>{
+		techStack()
+	})
+	const more = document.getElementById("more").addEventListener("click",()=>{
+		moreMe()
+	})
   } 
   
   function draw() { 
+	
 	//get the pencil effect by using mouseX/Y and pmouseX/Y (previous) inbuilt positions connecting where the mouse current is and where it was
 	line(mouseX, mouseY, pmouseX, pmouseY);//line command takes 4 arguments starting x&y and ending x&y
 	//ellipse(mouseX, mouseY, 20, 20);
 	  
+  }
+
+  function education(){
+	const textAbout=document.getElementById("textAbout")
+	const education =`Barchelotte Degree in Computer Science,UNAM`
+	textAbout.textContent=education
+	
+
+  }
+
+  function techStack(){
+	const textAbout=document.getElementById("textAbout")
+	const stack = `My favourite Stack:`
+	textAbout.textContent=stack
+
+  }
+
+  function moreMe(){
+	const textAbout=document.getElementById("textAbout")
+	const textMore = `Help! Im a  designer \ lover cat \ lover plant \ writter \ trapped in a programmer body`
+	textAbout.textContent= textMore
   }
